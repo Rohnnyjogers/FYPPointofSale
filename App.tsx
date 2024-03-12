1351b8fc61dd0d93e8d71613dce069d80294ac81
Johnny <johnnyrogers29@gmail.com>
2024-03-12 16:19:13 +0000

    c1: Add initial project.

--- App.tsx
+++ App.tsx
@@ -1,118 +1,251 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Button, Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import NfcManager, { Ndef, NfcTech, } from 'react-native-nfc-manager';
+import { Item, foodList, drinkList } from './src/data/Products';
+const App = () => {
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  const [foodItems, setFoodItems] = useState<Item[]>(foodList);
+  const [drinkItems, setDrinkItems] = useState<Item[]>(drinkList);
+  const [productList, setProductList] = useState<Item[]>([...foodList, ...drinkList]);
+  const [receipt, setReceipt] = useState<Item[]>([]);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const addProductToReceipt = (product: string) => {
+    const itemSelected = productList.find((item) => item.description === product)
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+    if(itemSelected){
+      
+      const existingItem = receipt.find((item) => item.description === product);
+      
+      if(existingItem){
+        setReceipt((currentReceipt) =>
+          currentReceipt.map((item) => 
+            item.description === product ? 
+              {...item, quantity: item.quantity + 1} : item));
+      } else{
+        setReceipt((prev) => ([...prev, itemSelected]));
+      }
+    } else{
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+      if(!itemSelected){
+        console.log('Item not found');
+      } else{
+        setReceipt((prev) => [...prev, {...(itemSelected as Item), quantity: 1}]);
+      }
+    }
+  }
+
+  const receiptSubTotal = () => {
+    const subTotal = receipt.reduce((acc, itm) => {
+      const itemSubTotal = itm.quantity * itm.price;
+      return acc + itemSubTotal;
+    }, 0);
+
+    return subTotal;
+  }
+
+  const receiptItemsTotal = () => {
+    const itemsTotal = receipt.reduce((acc, itm) => {
+      const quantity = itm.quantity;
+      return acc + quantity;
+    }, 0);
+
+    return itemsTotal;
+  }
+
+  const clearTransaction = () => {
+    setReceipt(prev => []);
+  }
+
+  const writeNfc = async() => {
+
+    const vendorName: string = 'The Corner Shop';
+    const vendorId: string = 'V123654';
+    const receiptId: string = '1122235';
+    //location
+    const items: Item[] = receipt;
+    const priceTotal: number = receiptSubTotal();
+    const itemsTotal: number = receiptItemsTotal();
+
+    const receiptData = {
+      vendorName,
+      vendorId,
+      receiptId,
+      items,
+      priceTotal,
+      itemsTotal
+    }
+
+    const jsonStringReceipt = JSON.stringify(receiptData);
+    console.log(jsonStringReceipt);
+
+    try{
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      // const hasNdef = NfcManager.requestTechnology(NfcTech.Ndef);
+      // if(hasNdef != null){
+      // }
+      
+      const bytes = Ndef.encodeMessage([Ndef.textRecord(jsonStringReceipt)]);
+      console.log('Encoded bytes value:', bytes);
+      console.log('Decoded bytes value:', Ndef.decodeMessage(bytes));
+
+      if(bytes){
+        await NfcManager.ndefHandler.writeNdefMessage(bytes);
+      }
+    }
+    catch(error){
+      console.log('Nfc Manager error:', error);
+    }
+    finally{
+      NfcManager.cancelTechnologyRequest();
+    }
+  }
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <View style={styles.container}>
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        showsVerticalScrollIndicator={false}>
+        <Text style={styles.appTitle}>spence POS</Text>
+        <View style={styles.productsContainer}>
+          <Text style={styles.title}>Food items</Text>
+          <View style={styles.itemContainer}>
+            {foodItems.map((product) =>
+              <View key={product.description}>
+                <Button
+                  title={product.description}
+                  onPress={() => 
+                  addProductToReceipt(product.description)}
+                />
+              </View>
+            )}
+          </View>
+            
+          <Text style={styles.title}>Drink items</Text>
+          <View style={styles.itemContainer}>
+            {drinkItems.map((product) =>
+              <View key={product.description}>
+                <Button
+                  title={product.description}
+                  onPress={() => 
+                  addProductToReceipt(product.description)}
+                />
+              </View>
+            )}      
+          </View>
+        </View>
+
+        <View style={styles.transactionContainer}>
+        <Text style={styles.title}>Transaction</Text>
+        <View style={styles.receiptContainer}>
+          <ScrollView>
+            <View style={styles.itemListContainer}>
+              {receipt.map((item) =>
+                  <View style={styles.receiptItems} key={item.description}>
+                    <Text>{item.quantity}</Text>
+                    <Text> x </Text>
+                    <Text>{item.description}</Text>
+                    <Text>€{item.price}</Text>
+                  </View>
+              )}
+            </View>
+            
+            <View style={styles.totalsContainer}>
+              <View style={styles.divider}/>
+              <View style={styles.subtotalContainer}>
+                <Text style={styles.subtotal}>Sub Total:</Text>
+                <Text style={styles.subtotalValue}>€{receiptSubTotal()}</Text>
+              </View>
+            </View>
+          </ScrollView>
+        </View>
+        </View>
+
+        <View style={styles.buttonContainer}>
+            <Button
+              title='Apply Transaction'
+              onPress={writeNfc}
+            />
+
+            <Button
+              title='Clear Transaction'
+              onPress={clearTransaction}
+            />
         </View>
       </ScrollView>
-    </SafeAreaView>
-  );
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+const styles = StyleSheet.create({
+  appTitle: {
+    marginTop: 4,
+    fontSize: 24
+  },
+  container: {
+    flex: 1,
+    paddingStart: 20,
+    paddingEnd: 20
+  },
+  productsContainer: {
+    marginTop: 40
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    gap: 10,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 4
+  },
+  title: {
+    marginTop: 10,
+    fontSize: 18,
+  },
+  transactionContainer: {
+    marginTop: 40
+  },
+  itemListContainer: {
+    flex: 1,
+    height: Dimensions.get('window').height*0.32,
+  },
+  receiptContainer: {
+    flex: 1,
+    height: Dimensions.get('window').height*0.4,
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingTop: 10
+  },
+  receiptItems: {
+    marginStart: 10,
+    marginEnd: 10,
+    flexDirection: 'row',
+    gap: 10
+  },
+  buttonContainer: {
+    marginTop: 20,
+    rowGap: 10,
+    marginBottom: 20
+  },
+  totalsContainer: {
+    alignSelf: 'baseline'
+  },
+  divider: {
+   height: 1,
+   width: Dimensions.get('window').width*0.9,
+   backgroundColor: '#37474F'
+  },
+  subtotalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  subtotal: {
+    fontSize: 18,
+    padding: 12.5
+  },
+  subtotalValue: {
+    fontSize: 18,
+    padding: 12.5
+  }
+})
---
+++ src/data/Products.ts
@@ -0,0 +1,93 @@
+
+export interface Item {
+    description: string;
+    price: number;
+    quantity: number;
+}
+
+export const drinkList: Item[] = [
+    {
+        description: 'Water',
+        price: 1.2,
+        quantity: 1
+    },
+    {
+        description: 'Juice',
+        price: 1.5,
+        quantity: 1
+    },
+    {
+        description: 'Cola',
+        price: 1.8,
+        quantity: 1
+    },
+    {
+        description: 'Tea',
+        price: 1.8,
+        quantity: 1
+    },
+    {
+        description: 'Americano',
+        price: 2,
+        quantity: 1
+    },
+    {
+        description: 'Latte',
+        price: 3.5,
+        quantity: 1
+    },
+    {
+        description: 'Beer',
+        price: 3.5,
+        quantity: 1
+    },
+    {
+        description: 'Wine',
+        price: 4.5,
+        quantity: 1
+    },
+    
+];
+
+export const foodList: Item[] = [
+    {
+        description: 'Apple',
+        price: 0.5,
+        quantity: 1
+    },
+    {
+        description: 'Orange',
+        price: 0.5,
+        quantity: 1
+    },
+    {
+        description: 'Banana',
+        price: 0.5,
+        quantity: 1
+    },
+    {
+        description: 'Crisps',
+        price: 1,
+        quantity: 1
+    },
+    {
+        description: 'Chocolate',
+        price: 1,
+        quantity: 1
+    },
+    {
+        description: 'Nuts',
+        price: 1.2,
+        quantity: 1
+    },
+    {
+        description: 'Sandwich',
+        price: 3,
+        quantity: 1
+    },
+    {
+        description: 'Wrap',
+        price: 3,
+        quantity: 1
+    }
+];
